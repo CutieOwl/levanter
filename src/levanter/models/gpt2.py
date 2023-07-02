f1151f9e42c7f5c0a0b606ab17f177752b64d4b0
CutieOwl <kml11@live.com>
2023-07-02 15:32:34 -0700

    remove alibi printing

--- src/levanter/models/gpt2.py
+++ src/levanter/models/gpt2.py
@@ -481,14 +481,14 @@ class Gpt2LMHeadModel(TorchSerializationMixin, eqx.Module):
         self.attn_heads = config.num_heads
         slopes = jnp.array(get_slopes(self.attn_heads))
         self.alibi = jnp.expand_dims(jnp.expand_dims(slopes, 1), 1) * jnp.expand_dims(jnp.expand_dims(jnp.arange(maxpos), 0), 0).repeat(self.attn_heads, axis=0)
-        print("alibi 1", np.array(jax.device_get(self.alibi)))
-        print("alibi shape 1", self.alibi.shape)
+        #print("alibi 1", np.array(jax.device_get(self.alibi)))
+        #print("alibi shape 1", self.alibi.shape)
         self.alibi = self.alibi.reshape(self.attn_heads, 1, maxpos)
-        print("alibi 2", np.array(jax.device_get(self.alibi)))
-        print("alibi shape 2", self.alibi.shape)
+        #print("alibi 2", np.array(jax.device_get(self.alibi)))
+        #print("alibi shape 2", self.alibi.shape)
         self.alibi = jnp.tile(self.alibi, (config.seq_len // maxpos, 1, 1))
-        print("alibi 3", np.array(jax.device_get(self.alibi)))
-        print("alibi shape 3", self.alibi.shape)
+        #print("alibi 3", np.array(jax.device_get(self.alibi)))
+        #print("alibi shape 3", self.alibi.shape)
 
     def __call__(self, input_ids: NamedArray, attn_mask: Optional[NamedArray], *, inference, key):
         if not inference and key is None:
@@ -505,7 +505,7 @@ class Gpt2LMHeadModel(TorchSerializationMixin, eqx.Module):
         print("attn arr shape", attn_arr.shape)
         attn_arr = attn_arr[:hidden_states.array.shape[0] * self.attn_heads, :dim, :dim]
         print("attn arr shape final", np.array(jax.device_get(attn_arr)))
-        jax.debug.print("end attn arr {}", attn_arr)
+        print('original attn arr', np.array(jax.device_get(attn_arr)))
         attn_mask = NamedArray(attn_arr, attn_mask.axes)
     
         hidden_states = self.transformer(hidden_states, attn_mask, inference=inference, key=k_transformer)
