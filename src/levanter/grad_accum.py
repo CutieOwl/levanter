52e6343de4f889ad3040d4707cb442872cfc4aed
CutieOwl <kml11@live.com>
2023-07-31 18:39:09 -0700

    tuple doesn't have shape

--- src/levanter/grad_accum.py
+++ src/levanter/grad_accum.py
@@ -91,7 +91,7 @@ def accumulate_gradients_sharded(
     print("accum step", AccumStep)
     # second, we want to reshape our data to (num_micro_steps, micro_batch_size, ...), sharded along the data axis
     inputs = _reshape_for_microbatch(Batch, Microbatch, AccumStep, inputs, parameter_axis_mapping)
-    print('inputs shape', inputs.shape)
+    print('inputs shape', len(inputs))
     print("inputs after reshape", inputs)
     input_key = inputs[2]
     inputs = inputs[:2]
